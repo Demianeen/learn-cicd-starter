f78217a20ad042eb90328df22952c9558d8b5f22
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-06-12 09:21:58 +0100

    test: add tests for GetApiKey

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,41 @@
+package auth_test
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+)
+
+func TestGetApiKey(t *testing.T) {
+	tests := map[string]struct {
+		input http.Header
+		sep   string
+		want  string
+		err   bool
+	}{
+		"valid header": {input: http.Header{
+			"Authorization": []string{"ApiKey test"},
+		}, want: "test"},
+		"empty header": {input: http.Header{}, err: true},
+		"invalid auth scheme": {input: http.Header{
+			"Authorization": []string{"Broken test"},
+		}, err: true},
+		"no auth token, but scheme is valid": {input: http.Header{
+			"Authorization": []string{"ApiKey"},
+		}, err: true},
+	}
+
+	for testName, tc := range tests {
+		t.Run(testName, func(t *testing.T) {
+			got, err := auth.GetAPIKey(tc.input)
+			if err != nil && tc.err != true {
+				t.Fatalf("expected to not error, got: %v", err)
+			}
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Fatalf("expected: %v, got: %v", tc.want, got)
+			}
+		})
+	}
+}
